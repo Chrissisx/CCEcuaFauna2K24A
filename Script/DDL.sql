9056aa6b5dd36d48e49705938b98e99b936738c2
Chrissisx <chriscriollo2309@gmail.com>
2024-08-16 03:00:16 -0500

    Actu a los .sql

--- Script/DDL.sql
+++ Script/DDL.sql
@@ -1,197 +1,122 @@
 --database: ../DataBase/CCTabla.sqlite
 
-DROP TABLE IF EXISTS CCHormiga;
+/*
+|-----------------------------------------------|
+| (©) 2K24, All rights reserved.                |
+| chriscriollo2309@gmail.com        xchrisis    |
+|-----------------------------------------------|
+Autor : Chris 
+Fecha : 16.agosto.2k24
+Script: Creando Tablas Hormiga
+*/
+
 DROP TABLE IF EXISTS CCHormigaAlimento;
+DROP TABLE IF EXISTS CCHormiga;
 DROP TABLE IF EXISTS CCAlimento;
 DROP TABLE IF EXISTS CCGenoAlimento;
 DROP TABLE IF EXISTS CCIngestaNativa;
 DROP TABLE IF EXISTS CCEstado;
 DROP TABLE IF EXISTS CCSexo;
 DROP TABLE IF EXISTS CCTipoHormiga;
-
 DROP TABLE IF EXISTS CCProvincia;
 DROP TABLE IF EXISTS CCPais;
 DROP TABLE IF EXISTS CCRegion;
 
 CREATE TABLE CCRegion (
-        CodigoRegion      INTEGER     NOT NULL PRIMARY KEY AUTOINCREMENT     
-        ,NombreRegion       VARCHAR(30) NOT NULL UNIQUE
-        ,Estado         VARCHAR(1)  NOT NULL DEFAULT('A')
-        ,FechaCrea      DATETIME    DEFAULT(datetime('now','localtime'))
-        ,FechaModifica  DATETIME
+        CodigoRegion      INTEGER     NOT NULL PRIMARY KEY AUTOINCREMENT,
+        NombreRegion      VARCHAR(30) NOT NULL UNIQUE,
+        Estado            VARCHAR(1)  NOT NULL DEFAULT 'A',
+        FechaCrea         DATETIME    DEFAULT(datetime('now','localtime')),
+        FechaModifica     DATETIME
 );
-
-INSERT INTO CCRegion (NombreRegion)
-VALUES 
-("Costa"),
-("Sierra"),
-("Oriente"),
-("Galapagos");
-
 
 CREATE TABLE CCPais (
-        CodigoPais      INTEGER     NOT NULL PRIMARY KEY AUTOINCREMENT     
-        ,NombrePais       VARCHAR(30) NOT NULL UNIQUE
-        ,Estado         VARCHAR(1)  NOT NULL DEFAULT('A')
-        ,FechaCrea      DATETIME    DEFAULT(datetime('now','localtime'))
-        ,FechaModifica  DATETIME
+        CodigoPais        INTEGER     NOT NULL PRIMARY KEY AUTOINCREMENT,
+        NombrePais        VARCHAR(30) NOT NULL UNIQUE,
+        Estado            VARCHAR(1)  NOT NULL DEFAULT 'A',
+        FechaCrea         DATETIME    DEFAULT(datetime('now','localtime')),
+        FechaModifica     DATETIME
 );
-
-INSERT INTO CCPais (NombrePais)
-VALUES 
-("Ecuador");
 
 CREATE TABLE CCProvincia (
-        CodigoProvincia      INTEGER     NOT NULL PRIMARY KEY AUTOINCREMENT     
-        ,NombreProvincia       VARCHAR(30) NOT NULL UNIQUE
-        ,CodigoPais     INTEGER NOT NULL
-        ,CodigoRegion INTEGER NOT NULL
-        ,Estado         VARCHAR(1)  NOT NULL DEFAULT('A')
-        ,FechaCrea      DATETIME    DEFAULT(datetime('now','localtime'))
-        ,FechaModifica  DATETIME
+        CodigoProvincia   INTEGER     NOT NULL PRIMARY KEY AUTOINCREMENT,
+        NombreProvincia   VARCHAR(30) NOT NULL UNIQUE,
+        CodigoPais        INTEGER     NOT NULL,
+        CodigoRegion      INTEGER     NOT NULL,
+        Estado            VARCHAR(1)  NOT NULL DEFAULT 'A',
+        FechaCrea         DATETIME    DEFAULT(datetime('now','localtime')),
+        FechaModifica     DATETIME
 );
-
-INSERT INTO CCProvincia (NombreProvincia, CodigoPais, CodigoRegion)
-VALUES 
--- Costa (CodigoRegion = 1)
-("El Oro", 1, 1),
-("Esmeraldas", 1, 1),
-("Guayas", 1, 1),
-("Los Ríos", 1, 1),
-("Manabí", 1, 1),
-("Santa Elena", 1, 1),
-("Santo Domingo de los Tsáchilas", 1, 1),
-
--- Sierra (CodigoRegion = 2)
-("Azuay", 1, 2),
-("Bolívar", 1, 2),
-("Cañar", 1, 2),
-("Carchi", 1, 2),
-("Chimborazo", 1, 2),
-("Cotopaxi", 1, 2),
-("Imbabura", 1, 2),
-("Loja", 1, 2),
-("Pichincha", 1, 2),
-("Tungurahua", 1, 2),
-
--- Oriente (CodigoRegion = 3)
-("Morona Santiago", 1, 3),
-("Napo", 1, 3),
-("Orellana", 1, 3),
-("Pastaza", 1, 3),
-("Sucumbíos", 1, 3),
-("Zamora Chinchipe", 1, 3),
-
--- Galápagos (CodigoRegion = 4)
-("Galápagos", 1, 4);
-
 
 CREATE TABLE CCTipoHormiga (
-        CodigoTipoHormiga      INTEGER     NOT NULL PRIMARY KEY AUTOINCREMENT     
-        ,NombreTipoHormiga       VARCHAR(30) NOT NULL UNIQUE
-        ,Estado         VARCHAR(1)  NOT NULL DEFAULT('A')
-        ,FechaCrea      DATETIME    DEFAULT(datetime('now','localtime'))
-        ,FechaModifica  DATETIME
+        CodigoTipoHormiga INTEGER     NOT NULL PRIMARY KEY AUTOINCREMENT,
+        NombreTipoHormiga VARCHAR(30) NOT NULL UNIQUE,
+        Estado            VARCHAR(1)  NOT NULL DEFAULT 'A',
+        FechaCrea         DATETIME    DEFAULT(datetime('now','localtime')),
+        FechaModifica     DATETIME
 );
-
-INSERT INTO CCTipoHormiga (NombreTipoHormiga)
-VALUES 
-("Larva"),
-("Soldado"),
-("Rastrera"),
-("Reina");
 
 CREATE TABLE CCSexo (
-        CodigoSexo      INTEGER     NOT NULL PRIMARY KEY AUTOINCREMENT     
-        ,NombreSexo       VARCHAR(15) NOT NULL UNIQUE
-        ,Estado         VARCHAR(1)  NOT NULL DEFAULT('A')
-        ,FechaCrea      DATETIME    DEFAULT(datetime('now','localtime'))
-        ,FechaModifica  DATETIME
+        CodigoSexo        INTEGER     NOT NULL PRIMARY KEY AUTOINCREMENT,
+        NombreSexo        VARCHAR(15) NOT NULL UNIQUE,
+        Estado            VARCHAR(1)  NOT NULL DEFAULT 'A',
+        FechaCrea         DATETIME    DEFAULT(datetime('now','localtime')),
+        FechaModifica     DATETIME
 );
-
-INSERT INTO CCSexo (NombreSexo)
-VALUES 
-("Macho"),
-("Hembra"),
-("Asexual");
-
 
 CREATE TABLE CCEstado (
-        CodigoEstado      INTEGER     NOT NULL PRIMARY KEY AUTOINCREMENT     
-        ,NombreEstado       VARCHAR(20) NOT NULL UNIQUE
-        ,Estado         VARCHAR(1)  NOT NULL DEFAULT('A')
-        ,FechaCrea      DATETIME    DEFAULT(datetime('now','localtime'))
-        ,FechaModifica  DATETIME
+        CodigoEstado      INTEGER     NOT NULL PRIMARY KEY AUTOINCREMENT,
+        NombreEstado      VARCHAR(20) NOT NULL UNIQUE,
+        Estado            VARCHAR(1)  NOT NULL DEFAULT 'A',
+        FechaCrea         DATETIME    DEFAULT(datetime('now','localtime')),
+        FechaModifica     DATETIME
 );
-
-INSERT INTO CCEstado (NombreEstado)
-VALUES 
-("Huevo"),
-("Larva"),
-("Pupa"),
-("Adulta");
 
 CREATE TABLE CCIngestaNativa (
-        CodigoIngestaNativa      INTEGER     NOT NULL PRIMARY KEY AUTOINCREMENT     
-        ,NombreIngestaNativa       VARCHAR(20) NOT NULL UNIQUE
-        ,Estado         VARCHAR(1)  NOT NULL DEFAULT('A')
-        ,FechaCrea      DATETIME    DEFAULT(datetime('now','localtime'))
-        ,FechaModifica  DATETIME
+        CodigoIngestaNativa INTEGER   NOT NULL PRIMARY KEY AUTOINCREMENT,
+        NombreIngestaNativa VARCHAR(20) NOT NULL UNIQUE,
+        Estado              VARCHAR(1)  NOT NULL DEFAULT 'A',
+        FechaCrea           DATETIME    DEFAULT(datetime('now','localtime')),
+        FechaModifica       DATETIME
 );
-
-INSERT INTO CCIngestaNativa (NombreIngestaNativa)
-VALUES 
-("Carnivoro"),
-("Herbivoro"),
-("Omnivoro"),
-("Insectivoro");
 
 CREATE TABLE CCGenoAlimento (
-        CodigoGenoAlimento      INTEGER     NOT NULL PRIMARY KEY AUTOINCREMENT     
-        ,NombreGenoAlimento       VARCHAR(20) NOT NULL UNIQUE
-        ,Estado         VARCHAR(1)  NOT NULL DEFAULT('A')
-        ,FechaCrea      DATETIME    DEFAULT(datetime('now','localtime'))
-        ,FechaModifica  DATETIME
+        CodigoGenoAlimento INTEGER     NOT NULL PRIMARY KEY AUTOINCREMENT,
+        NombreGenoAlimento VARCHAR(20) NOT NULL UNIQUE,
+        Estado            VARCHAR(1)  NOT NULL DEFAULT 'A',
+        FechaCrea         DATETIME    DEFAULT(datetime('now','localtime')),
+        FechaModifica     DATETIME
 );
-
-INSERT INTO CCGenoAlimento (NombreGenoAlimento)
-VALUES 
-("X"),
-("XX"),
-("XY");
 
 CREATE TABLE CCAlimento (
-        CodigoAlimento      INTEGER     NOT NULL PRIMARY KEY AUTOINCREMENT     
-        ,NombreAlimento       VARCHAR(20) NOT NULL UNIQUE
-        ,CodigoIngestaNativa INTEGER     NOT NULL  
-        ,CodigoGenoAlimento INTEGER     NOT NULL 
-        ,DescripcionAlimento  VARCHAR(500) NOT NULL
-        ,Estado         VARCHAR(1)  NOT NULL DEFAULT('A')
-        ,FechaCrea      DATETIME    DEFAULT(datetime('now','localtime'))
-        ,FechaModifica  DATETIME
+        CodigoAlimento     INTEGER     NOT NULL PRIMARY KEY AUTOINCREMENT,
+        NombreAlimento     VARCHAR(20) NOT NULL UNIQUE,
+        CodigoIngestaNativa INTEGER    NOT NULL,
+        CodigoGenoAlimento INTEGER     NOT NULL,
+        DescripcionAlimento VARCHAR(500) NOT NULL,
+        Estado            VARCHAR(1)  NOT NULL DEFAULT 'A',
+        FechaCrea         DATETIME    DEFAULT(datetime('now','localtime')),
+        FechaModifica     DATETIME
 );
-
 
 CREATE TABLE CCHormigaAlimento (
-        CodigoHormigaAlimento      INTEGER     NOT NULL PRIMARY KEY AUTOINCREMENT     
-        ,CodigoHormiga       INTEGER NOT NULL
-        ,CodigoAlimento       INTEGER NOT NULL
-        ,Estado         VARCHAR(1)  NOT NULL DEFAULT('A')
-        ,FechaCrea      DATETIME    DEFAULT(datetime('now','localtime'))
-        ,FechaModifica  DATETIME
+        CodigoHormigaAlimento INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT,
+        CodigoHormiga        INTEGER   NOT NULL,
+        CodigoAlimento       INTEGER   NOT NULL,
+        Estado              VARCHAR(1) NOT NULL DEFAULT 'A',
+        FechaCrea           DATETIME   DEFAULT(datetime('now','localtime')),
+        FechaModifica       DATETIME
 );
-
 
 CREATE TABLE CCHormiga (
-        CodigoHormiga      INTEGER     NOT NULL PRIMARY KEY AUTOINCREMENT     
-        ,NombreHormiga       VARCHAR(30) NOT NULL UNIQUE
-        ,CodigoSexo INTEGER NOT NULL
-        ,CodigoTipoHormiga INTEGER NOT NULL
-        ,CodigoEstado INTEGER NOT NULL
-        ,CodigoProvincia INTEGER NOT NULL
-        ,CodigoAlimento INTEGER NOT NULL
-        ,Estado         VARCHAR(1)  NOT NULL DEFAULT('A')
-        ,FechaCrea      DATETIME    DEFAULT(datetime('now','localtime'))
-        ,FechaModifica  DATETIME
+        CodigoHormiga      INTEGER     NOT NULL PRIMARY KEY AUTOINCREMENT,
+        NombreHormiga      VARCHAR(30) NOT NULL UNIQUE,
+        CodigoSexo         INTEGER     NOT NULL,
+        CodigoTipoHormiga  INTEGER     NOT NULL,
+        CodigoEstado       INTEGER     NOT NULL,
+        CodigoProvincia    INTEGER     NOT NULL,
+        CodigoAlimento     INTEGER     NOT NULL,
+        Estado            VARCHAR(1)  NOT NULL DEFAULT 'A',
+        FechaCrea         DATETIME    DEFAULT(datetime('now','localtime')),
+        FechaModifica     DATETIME
 );
-
---
+++ Script/DML.sql
@@ -0,0 +1,82 @@
+--database: ../DataBase/CCTabla.sqlite
+
+/*
+|-----------------------------------------------|
+| (©) 2K24, All rights reserved.                |
+| chriscriollo2309@gmail.com        xchrisis    |
+|-----------------------------------------------|
+Autor : Chris 
+Fecha : 16.agosto.2k24
+Script: Insertando Datos Hormiga
+*/
+
+INSERT INTO CCRegion 
+( NombreRegion   ) VALUES 
+( "Costa"        ),
+( "Sierra"       ),
+( "Oriente"      ),
+( "Galapagos"    );
+
+INSERT INTO CCPais 
+( NombrePais     ) VALUES 
+( "Ecuador"      );
+
+INSERT INTO CCProvincia 
+( NombreProvincia                   , CodigoPais , CodigoRegion ) VALUES 
+( "El Oro"                          , 1          , 1            ),
+( "Esmeraldas"                      , 1          , 1            ),
+( "Guayas"                          , 1          , 1            ),
+( "Los Ríos"                        , 1          , 1            ),
+( "Manabí"                          , 1          , 1            ),
+( "Santa Elena"                     , 1          , 1            ),
+( "Santo Domingo de los Tsáchilas"  , 1          , 1            ),
+( "Azuay"                           , 1          , 2            ),
+( "Bolívar"                         , 1          , 2            ),
+( "Cañar"                           , 1          , 2            ),
+( "Carchi"                          , 1          , 2            ),
+( "Chimborazo"                      , 1          , 2            ),
+( "Cotopaxi"                        , 1          , 2            ),
+( "Imbabura"                        , 1          , 2            ),
+( "Loja"                            , 1          , 2            ),
+( "Pichincha"                       , 1          , 2            ),
+( "Tungurahua"                      , 1          , 2            ),
+( "Morona Santiago"                 , 1          , 3            ),
+( "Napo"                            , 1          , 3            ),
+( "Orellana"                        , 1          , 3            ),
+( "Pastaza"                         , 1          , 3            ),
+( "Sucumbíos"                       , 1          , 3            ),
+( "Zamora Chinchipe"                , 1          , 3            ),
+( "Galápagos"                       , 1          , 4            );
+
+INSERT INTO CCTipoHormiga 
+( NombreTipoHormiga ) VALUES 
+( "Larva"           ),
+( "Soldado"         ),
+( "Rastrera"        ),
+( "Reina"           );
+
+INSERT INTO CCSexo 
+( NombreSexo       ) VALUES 
+( "Macho"          ),
+( "Hembra"         ),
+( "Asexual"        );
+
+INSERT INTO CCEstado 
+( NombreEstado     ) VALUES 
+( "Huevo"          ),
+( "Larva"          ),
+( "Pupa"           ),
+( "Adulta"         );
+
+INSERT INTO CCIngestaNativa 
+( NombreIngestaNativa  ) VALUES 
+( "Carnivoro"          ),
+( "Herbivoro"          ),
+( "Omnivoro"           ),
+( "Insectivoro"        );
+
+INSERT INTO CCGenoAlimento 
+( NombreGenoAlimento   ) VALUES 
+( "X"                  ),
+( "XX"                 ),
+( "XY"                 );
